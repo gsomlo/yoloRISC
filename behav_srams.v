9d9c5a6344f77eeba4a8d416ac6f8ac1b3020afd
Gabriel L. Somlo <gsomlo@gmail.com>
2019-01-30 19:19:56 -0500

    Reduce main memory size to 1K
    The memory is specified at 256MB, but the demo firmware is a little
    less than 512 bytes in size. If we add a 512-byte stack, we only
    need 1K of *actual* memory.
    
    Let's only use 7 bits ([6:0]) of the 25-bit address lines, and wrap
    around if accesses outside 0x8000_0000..0x8000_0400 should ever occur.

--- behav_srams.v
+++ behav_srams.v
@@ -1,4 +1,5 @@
-// totals 256MB (too large to synthesize!!!)
+// - specified at 256MB (too large to synthesize);
+// - we implement 1K (half for .text, half for stack):
 module mem_ext(
 	input         W0_clk,
 	input  [24:0] W0_addr,
@@ -11,32 +12,48 @@ module mem_ext(
 	output [63:0] R0_data
 );
 	reg [24:0] reg_R0_addr;
-	reg [63:0] ram [33554431:0];
+//	reg [63:0] ram [33554431:0];
+	reg [63:0] ram [127:0];
 	initial begin:B0
 		integer i;
-		for (i = 0; i < 33554432; i++)
+		for (i = 0; i < 128; i++)
 			ram[i] = 64'h0001_0001_0001_0001; // NOP: addi x0, x0, 0
+		// "li sp, 0x80000400" translates into the following opcodes:
+		//   0010011b addiw sp,zero,1
+		//   01f11113 slli  sp,sp,0x1f
+		//   40010113 addi  sp,sp,1024
+		// which should go into memory like this:
+		ram[0] = 64'h01f1_1113_0010_011b;
+		ram[1] = 64'h0001_0001_4001_0113;
 	end
 	always @(posedge R0_clk)
 		if (R0_en) begin
 			reg_R0_addr <= R0_addr;
 			$display("## mem_rd: a=%h; d=%h",
-				R0_addr, ram[R0_addr]);
+				R0_addr[6:0], ram[R0_addr[6:0]]);
 		end
 	always @(posedge W0_clk)
 		if (W0_en) begin
-			if (W0_mask[0]) ram[W0_addr][ 7: 0] <= W0_data[ 7: 0];
-			if (W0_mask[1]) ram[W0_addr][15: 8] <= W0_data[15: 8];
-			if (W0_mask[2]) ram[W0_addr][23:16] <= W0_data[23:16];
-			if (W0_mask[3]) ram[W0_addr][31:24] <= W0_data[31:24];
-			if (W0_mask[4]) ram[W0_addr][39:32] <= W0_data[39:32];
-			if (W0_mask[5]) ram[W0_addr][47:40] <= W0_data[47:40];
-			if (W0_mask[6]) ram[W0_addr][55:48] <= W0_data[55:48];
-			if (W0_mask[7]) ram[W0_addr][63:56] <= W0_data[63:56];
+			if (W0_mask[0])
+				ram[W0_addr[6:0]][ 7: 0] <= W0_data[ 7: 0];
+			if (W0_mask[1])
+				ram[W0_addr[6:0]][15: 8] <= W0_data[15: 8];
+			if (W0_mask[2])
+				ram[W0_addr[6:0]][23:16] <= W0_data[23:16];
+			if (W0_mask[3])
+				ram[W0_addr[6:0]][31:24] <= W0_data[31:24];
+			if (W0_mask[4])
+				ram[W0_addr[6:0]][39:32] <= W0_data[39:32];
+			if (W0_mask[5])
+				ram[W0_addr[6:0]][47:40] <= W0_data[47:40];
+			if (W0_mask[6])
+				ram[W0_addr[6:0]][55:48] <= W0_data[55:48];
+			if (W0_mask[7])
+				ram[W0_addr[6:0]][63:56] <= W0_data[63:56];
 			$display("## mem_wr: a=%h d=%h [%b]",
-				W0_addr, W0_data, W0_mask);
+				W0_addr[6:0], W0_data, W0_mask);
 		end
-	assign R0_data = ram[reg_R0_addr];
+	assign R0_data = ram[reg_R0_addr[6:0]];
 endmodule
 
 // totals 4KB (does NOT match 0x2000_0000 size in subsystem/Configs.scala)
